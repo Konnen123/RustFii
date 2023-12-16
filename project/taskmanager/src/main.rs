dafe286abcb5b9a8b05f195a42a3be63b0315f01
Stefan <stefandoaga70@gmail.com>
2023-12-16 22:17:26 +0200

    Added tree view + solved cpu flickering

--- project/taskmanager/src/main.rs
+++ project/taskmanager/src/main.rs
@@ -1,8 +1,15 @@
+use std::collections::BTreeMap;
+
 use std::fs;
 use std::io;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+use std::time::Duration;
+
 
 use eframe::NativeOptions;
-use egui::Color32;
+
 use egui::FontId;
 use egui::RichText;
 use egui::Ui;
@@ -12,21 +19,115 @@ use std::str::FromStr;
 struct App {
     prev_total_cpu_time: f32,
     prev_idle_cpu_time: f32,
-    isFirstTime: bool,
-    processes_data: Vec<ProcInfo>,
+    is_list_mode: bool,
+    processes_data: BTreeMap<u32, ProcInfo>,
+    total_cpu_usage: Arc<Mutex<f32>>,
 }
 
 impl App {
-    fn new(cc: &eframe::CreationContext<'_>, process_data: Vec<ProcInfo>) -> Self {
+    fn new(cc: &eframe::CreationContext<'_>, process_data: BTreeMap<u32, ProcInfo>, mut total_cpu_usage: Arc<Mutex<f32>>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
-        Self {
+         Self {
             prev_total_cpu_time: 0.,
             prev_idle_cpu_time: 0.,
-            isFirstTime: true,
+            is_list_mode: true,
             processes_data: process_data,
+            total_cpu_usage: total_cpu_usage,
+        }
+        
+    }
+
+    fn create_header_row(&self, ui: &mut Ui) {
+        ui.horizontal(|ui| {
+            ui.columns(7, |columns| {
+                columns[0].label(
+                    RichText::new("Name").font(FontId::new(20., egui::FontFamily::Proportional)),
+                );
+                columns[1].label(
+                    RichText::new("User").font(FontId::new(20., egui::FontFamily::Proportional)),
+                );
+                columns[2].label(
+                    RichText::new("PID").font(FontId::new(20., egui::FontFamily::Proportional)),
+                );
+                columns[3].label(
+                    RichText::new("Status").font(FontId::new(20., egui::FontFamily::Proportional)),
+                );
+                columns[4].label(
+                    RichText::new("CPU%").font(FontId::new(20., egui::FontFamily::Proportional)),
+                );
+                columns[5].label(
+                    RichText::new("Mem").font(FontId::new(20., egui::FontFamily::Proportional)),
+                );
+                columns[6].label(
+                    RichText::new("Path").font(FontId::new(20., egui::FontFamily::Proportional)),
+                );
+            });
+        });
+    }
+    fn show_rows_as_list(&self, ui: &mut Ui) {
+        let text_style = egui::TextStyle::Body;
+        let row_height = ui.text_style_height(&text_style);
+        let total_rows = self.processes_data.len();
+
+        egui::ScrollArea::vertical().auto_shrink(false).show_rows(
+            ui,
+            row_height,
+            total_rows,
+            |ui: &mut Ui, total_rows: std::ops::Range<usize>| {
+                let process_vec: Vec<_> = self.processes_data.values().collect();
+                for i in total_rows {
+                    ui.horizontal(|ui| {
+                        ui.columns(7, |columns| {
+                            columns[0].label(RichText::new(format!("{}", process_vec[i].name)));
+                            columns[1].label(RichText::new(format!("{}", process_vec[i].user)));
+                            columns[2].label(RichText::new(format!("{}", process_vec[i].pid)));
+                            columns[3].label(RichText::new(format!("{}", process_vec[i].status)));
+                            columns[4].label(RichText::new(format!("{:.2}%", process_vec[i].cpu)));
+                            columns[5]
+                                .label(RichText::new(format!("{} kb", process_vec[i].memory_used)));
+                            columns[6].label(RichText::new(format!("{}", process_vec[i].path)));
+                        });
+                    });
+                }
+            },
+        );
+    }
+    fn show_rows_as_tree(&self, ui: &mut Ui) {
+        let text_style = egui::TextStyle::Body;
+        let row_height = ui.text_style_height(&text_style);
+        let total_rows = 0; //self.processes_data.len();
+        
+        egui::ScrollArea::vertical().auto_shrink(false).show_rows(
+            ui,
+            row_height,
+            total_rows,
+            |ui: &mut Ui, total_rows: std::ops::Range<usize>| {
+                for process in self.processes_data.values() {
+                    if process.is_children {
+                        continue;
+                    }
+                    self.create_collapse_area(ui, process)
+                }
+            },
+        );
+    }
+    fn create_collapse_area(&self, ui: &mut Ui, process: &ProcInfo) {
+        if process.children_processes.is_empty() {
+            ui.label(RichText::new(format!{"{} | {} | {} | {} | {:.2}% | {} kb | {}",process.name,process.user,process.pid,process.status,process.cpu,process.memory_used,process.path}));
+        } else {
+            ui.collapsing(RichText::new(format!{"{} | {} | {} | {} | {:.2}% | {} kb | {}",process.name,process.user,process.pid,process.status,process.cpu,process.memory_used,process.path}), |ui| {
+                
+                for child in &process.children_processes
+                {
+                    if let Some(child_process) = self.processes_data.get(child)
+                    {
+                        self.create_collapse_area(ui, child_process)
+                    }
+                }
+            });
         }
     }
 }
@@ -35,86 +136,32 @@ impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(&ctx, |ui| {
             ui.horizontal(|ui| {
-                ui.label(format!(
-                    "Total cpu usage: {:.2}%",
-                    read_cpu_usage(&mut self.prev_total_cpu_time, &mut self.prev_idle_cpu_time)
-                        .unwrap()
-                ));
+                if let Ok(locked_value) = self.total_cpu_usage.lock() {
+                    ui.label(format!("Total cpu usage: {:.2}%",locked_value));
+                }
+                else
+                {
+                    ui.label(format!("Unable to get cpu usage!"));
+                }
             });
+            let mut button_message = String::from("List view");
+            if self.is_list_mode {
+                button_message = String::from("Tree view");
+            }
 
-            ui.horizontal(|ui| {
-                ui.columns(7, |columns| {
-                    columns[0].label(
-                        RichText::new("Name")
-                            .font(FontId::new(20., egui::FontFamily::Proportional)),
-                    );
-                    columns[1].label(
-                        RichText::new("User")
-                            .font(FontId::new(20., egui::FontFamily::Proportional)),
-                    );
-                    columns[2].label(
-                        RichText::new("PID").font(FontId::new(20., egui::FontFamily::Proportional)),
-                    );
-                    columns[3].label(
-                        RichText::new("Status")
-                            .font(FontId::new(20., egui::FontFamily::Proportional)),
-                    );
-                    columns[4].label(
-                        RichText::new("CPU%")
-                            .font(FontId::new(20., egui::FontFamily::Proportional)),
-                    );
-                    columns[5].label(
-                        RichText::new("Mem").font(FontId::new(20., egui::FontFamily::Proportional)),
-                    );
-                    columns[6].label(
-                        RichText::new("Path")
-                            .font(FontId::new(20., egui::FontFamily::Proportional)),
-                    );
-                });
-            });
+            if ui.button(button_message).clicked() {
+                self.is_list_mode = !self.is_list_mode;
+            }
 
-            let text_style = egui::TextStyle::Body;
-            let row_height = ui.text_style_height(&text_style);
-            let total_rows = self.processes_data.len();
+            self.create_header_row(ui);
+            if self.is_list_mode {
+                self.show_rows_as_list(ui);
+            } else {
+                self.show_rows_as_tree(ui)
+            }
 
-            egui::ScrollArea::vertical().auto_shrink(false).show_rows(
-                ui,
-                row_height,
-                total_rows,
-                |ui: &mut Ui, total_rows: std::ops::Range<usize>| {
-                    for i in total_rows {
-
-                        ui.horizontal(|ui| {
-                            ui.columns(7, |columns| {
-                                columns[0].label(
-                                    RichText::new(format!("{}", &self.processes_data[i].name)),
-                                );
-                                columns[1].label(
-                                    RichText::new(format!("{}", &self.processes_data[i].user)),
-                                );
-                                columns[2].label(
-                                    RichText::new(format!("{}", &self.processes_data[i].pid)),
-                                );
-                                columns[3].label(
-                                    RichText::new(format!("{}", &self.processes_data[i].status)),
-                                );
-                                columns[4].label(
-                                    RichText::new(format!("{:.2}%", &self.processes_data[i].cpu)),
-                                );
-                                columns[5].label(
-                                    RichText::new(format!("{} kb", &self.processes_data[i].memory_used)),
-                                );
-                                columns[6].label(
-                                    RichText::new(format!("{}", &self.processes_data[i].path)),
-                                );
-                            });
-                        });
-                    }
-                },
-            );
+            ctx.request_repaint();
         });
-
-        // sleep(Duration::new(5, 0));
     }
 }
 
@@ -123,7 +170,9 @@ struct Info {
     name: String,
     status: String,
     memory_used: u32,
+    is_children: bool,
 }
+#[derive(Clone)]
 struct ProcInfo {
     name: String,
     user: String,
@@ -132,6 +181,8 @@ struct ProcInfo {
     cpu: f32,
     memory_used: u32,
     path: String,
+    children_processes: Vec<u32>,
+    is_children: bool,
 }
 //################################################################
 fn read_process_info(pid: u32) -> io::Result<Info> {
@@ -141,6 +192,7 @@ fn read_process_info(pid: u32) -> io::Result<Info> {
     let mut name = String::new();
     let mut status = String::new();
     let mut memory_used: u32 = 0;
+    let mut is_children: bool = false;
 
     for line in status_content.lines() {
         let parts: Vec<&str> = line.split_whitespace().collect();
@@ -150,6 +202,11 @@ fn read_process_info(pid: u32) -> io::Result<Info> {
                 "Name:" => name = parts[1].to_string(),
                 "State:" => status = parts[1].to_string(),
                 "VmRSS:" => memory_used = parts[1].parse().unwrap(),
+                "PPid:" => {
+                    if parts[1].parse::<u32>().unwrap() != 0 {
+                        is_children = true;
+                    }
+                }
                 _ => (),
             }
         }
@@ -160,6 +217,7 @@ fn read_process_info(pid: u32) -> io::Result<Info> {
         name,
         status,
         memory_used,
+        is_children,
     })
 }
 //################################################################
@@ -256,70 +314,100 @@ fn get_process_user_name(pid: u32) -> io::Result<String> {
     }
 }
 //################################################################
-fn get_children_processes(pid: u32) -> io::Result<String> {
-    let path = format!("/proc/{}/task/{}/children", pid, pid);
+fn get_children_processes(proc_info: &mut ProcInfo) -> io::Result<()> {
+    let path = format!("/proc/{}/task/{}/children", proc_info.pid, proc_info.pid);
     let children_processes = fs::read_to_string(path)?;
 
     if children_processes.is_empty() {
-        return Ok(format!("{}", pid));
+        return Ok(());
     }
-    let mut process_tree = format!("{}(", pid);
+
     for child in children_processes.split_whitespace() {
-        match get_children_processes(child.parse().unwrap()) {
-            Ok(child_tree) => process_tree.push_str(&child_tree),
-            Err(error) => {
-                println!("Error at get_child_process({}): {}", pid, error);
-            }
+        match child.parse::<u32>() {
+            Ok(child_pid) => proc_info.children_processes.push(child_pid),
+            Err(_) => println!("Error at getting child pid: {}!", child),
         }
-        process_tree.push(',');
     }
-    process_tree.pop().unwrap();
-    process_tree.push(')');
-    Ok(process_tree)
+
+    Ok(())
 }
 //################################################################
+fn get_process_data(pid: u32) -> ProcInfo {
+    let mut proc_info: ProcInfo = ProcInfo {
+        name: String::from(""),
+        user: String::from(""),
+        pid: 0,
+        status: String::from(""),
+        cpu: 0.,
+        memory_used: 0,
+        path: String::from(""),
+        children_processes: Vec::new(),
+        is_children: false,
+    };
+
+    if let Ok(info) = read_process_info(pid) {
+        proc_info.status = info.status;
+        proc_info.pid = info.pid;
+        proc_info.memory_used = info.memory_used;
+        proc_info.name = info.name;
+        proc_info.is_children = info.is_children;
+    }
+    if let Ok(process_cpu_usage) = get_process_cpu_usage(pid) {
+        proc_info.cpu = process_cpu_usage;
+    }
+    if let Ok(user_name) = get_process_user_name(pid) {
+        proc_info.user = user_name;
+    }
+    if let Ok(file_path) = get_process_file_path(pid) {
+        proc_info.path = file_path;
+    }
+    if let Err(error) = get_children_processes(&mut proc_info) {
+        println!("Error at get_children_processes: {}", error);
+    }
+
+    proc_info
+}
+//################################################################
+
 fn main() {
-    let mut processes_data: Vec<ProcInfo> = Vec::new();
+    let mut processes_data: BTreeMap<u32, ProcInfo> = BTreeMap::new();
 
     let proc_path = "/proc";
 
     if let Ok(entries) = fs::read_dir(proc_path) {
         for entry in entries.filter_map(|e| e.ok()) {
             if let Ok(pid) = entry.file_name().to_string_lossy().parse::<u32>() {
-                let mut proc_info: ProcInfo = ProcInfo {
-                    name: String::from(""),
-                    user: String::from(""),
-                    pid: 0,
-                    status: String::from(""),
-                    cpu: 0.,
-                    memory_used: 0,
-                    path: String::from(""),
-                };
-
-                if let Ok(info) = read_process_info(pid) {
-                    proc_info.status = info.status;
-                    proc_info.pid = info.pid;
-                    proc_info.memory_used = info.memory_used;
-                    proc_info.name = info.name;
-                }
-                if let Ok(process_cpu_usage) = get_process_cpu_usage(pid) {
-                    proc_info.cpu = process_cpu_usage;
-                }
-                if let Ok(user_name) = get_process_user_name(pid) {
-                    proc_info.user = user_name;
-                }
-                if let Ok(file_path) = get_process_file_path(pid) {
-                    proc_info.path = file_path;
-                }
-                processes_data.push(proc_info);
+                let proc_info = get_process_data(pid);
+                processes_data.insert(pid, proc_info);
             }
         }
     }
+    let total_cpu_usage_mutex = Arc::new(Mutex::new(0.));
+    let total_cpu_usage_mutex_clone = total_cpu_usage_mutex.clone();
+
+  
+    thread::spawn(move || {
+        let mut previous_cpu_usage = 0_f32;
+        let mut previous_idle_time = 0_f32;
+        let thread_total_cpu_usage = Arc::clone(&total_cpu_usage_mutex);
+        loop
+        {
+            if let Ok(total_cpu) = read_cpu_usage(&mut previous_cpu_usage, &mut previous_idle_time)
+            {
+                if let Ok(mut total_cpu_usage) = thread_total_cpu_usage.lock()
+                {
+                    *total_cpu_usage = total_cpu;
+                }
+            }
+            thread::sleep(Duration::new(2, 0));
+        }
+    });
+
     let native_options = NativeOptions::default();
     match eframe::run_native(
         "Task Manager",
         native_options,
-        Box::new(|cc| Box::new(App::new(cc, processes_data))),
+        Box::new(move |cc| Box::new(App::new(cc, processes_data, Arc::clone(&total_cpu_usage_mutex_clone)))),
     ) {
         Ok(()) => println!("Running!"),
         Err(error) => println!("Error: {}", error),
